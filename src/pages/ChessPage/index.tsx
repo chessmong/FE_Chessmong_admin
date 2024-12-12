a9165d905c3820644b82a6dde7e246d26c3bed32
MEGUMMY1 <cys10902@nate.com>
2024-12-12 19:34:55 +0900

    Feat: 제출 버튼 추가

--- src/pages/ChessPage/index.tsx
+++ src/pages/ChessPage/index.tsx
@@ -2,12 +2,17 @@ import { useRecoilState } from "recoil";
 import styles from "./ChessPage.module.scss";
 import ChessBoard from "../../components/ChessBoard";
 import { positionsState } from "../../states/positionsState";
+import Button from "../../components/Button";
 
 export default function ChessPage() {
   const [savedPositions, setSavedPositions] = useRecoilState(positionsState);
 
   const savePosition = (position: string) => {
     setSavedPositions((prev) => [position, ...prev]);
+  };
+
+  const handleSubmit = () => {
+    console.log("제출");
   };
 
   return (
@@ -21,6 +26,9 @@ export default function ChessPage() {
             {savedPositions.map((position, index) => (
               <div key={index}>{position}</div>
             ))}
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button onClick={handleSubmit}>제출</Button>
           </div>
         </section>
       </section>
