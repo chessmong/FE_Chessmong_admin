084a75bac4fe43f289d0858cec4583319db13fbb
MEGUMMY1 <cys10902@nate.com>
2024-12-13 23:36:27 +0900

    Feat: Button disabled 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,12 @@
 import styles from "./Button.module.scss";
 import { ButtonProps } from "./Button.types";
 
-export default function Button({ children, onClick }: ButtonProps) {
+export default function Button({ children, onClick, disabled }: ButtonProps) {
   return (
-    <div className={styles.container} onClick={onClick}>
+    <div
+      className={`${styles.container} ${disabled && styles.disabled}`}
+      onClick={!disabled ? onClick : undefined}
+    >
       {children}
     </div>
   );
